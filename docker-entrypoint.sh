653e387776c055070cea9931ffda06b5e0e53417
Angel <cuquet@users.noreply.github.com>
2018-12-01 12:36:17 +0100

    discard mariadb check on docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-#while true; do
-#    ./wait-for mariadb:3306 </dev/null
-#    if [ "$?" = 0 ]; then
-#        break
-#    fi
-#    >&2 echo "Mysql/MariaDB is unavailable - sleeping"
-#    sleep 1
-#done
-#>&2 echo "Mysql/MariaDB is up - continuing"
 until psql $DATABASE_URL -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
